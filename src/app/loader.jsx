e9e3813ce0a720fe934bbb177ca742aef29ec620
Larvey <mbuguakelvinmunyui@gmail.com>
2024-09-21 22:24:22 +0300

    added the sign in mobile responsive without JWT

---
+++ src/app/loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Loader} from 'react-loaders'
+const Load = () => {
+  return (
+    <>
+        <Loader type="line-spin-fade-loader" />
+    </>
+  )
+}
+
+export default Load
---
+++ src/components/Navigation/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="footer-element">
+        <div className="logo-element">
+
+        </div>
+        <div>
+            <article></article>
+        </div>
+        <div>
+            <article></article>
+        </div>
+        <div>
+            <article></article>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
